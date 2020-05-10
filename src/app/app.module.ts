bb5b458d1cb43e927787ad3c7a36273898ecd622
Jordan <jordandoan@hotmail.com>
2020-05-10 11:31:42 -0700

    created core module folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,25 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
-import { HttpClientModule } from '@angular/common/http';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatGridListModule } from '@angular/material/grid-list';
-
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+
 import { AccountModule } from './account/account.module';
 import { BudgetsModule } from './budgets/budgets.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { PersonComponent } from './about/person/person.component';
-
+import { CoreModule } from './core/core.module';
 
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, AboutComponent, PersonComponent],
   imports: [
     BrowserModule,
-    HttpClientModule,
+
     RouterModule.forRoot([
       { path: '', component: HomeComponent },
       { path: 'about', component: AboutComponent },
@@ -29,10 +26,10 @@ import { PersonComponent } from './about/person/person.component';
     AccountModule,
     BudgetsModule,
     BrowserAnimationsModule,
-    MatToolbarModule,
-    MatGridListModule,
+    CoreModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
+  
 })
 export class AppModule {}
---
+++ src/app/core/core.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatGridListModule } from '@angular/material/grid-list';
+
+
+
+const modules = [
+  CommonModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatButtonModule,
+  HttpClientModule,
+  MatToolbarModule,
+  MatGridListModule,
+  FormsModule,
+]
+@NgModule({
+  declarations: [],
+  imports: modules,
+  exports: modules
+})
+
+export class CoreModule { }

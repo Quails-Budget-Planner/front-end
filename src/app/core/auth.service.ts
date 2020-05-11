f47542676b6aa95f7917b4e34d236ba458fbadf5
Jordan <jordandoan@hotmail.com>
2020-05-10 18:14:50 -0700

    Auth guard works

--- src/app/core/auth.service.ts
+++ src/app/core/auth.service.ts
@@ -1,12 +1,11 @@
 import { Injectable } from '@angular/core';
-import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Injectable({
   providedIn: 'root',
 })
-export class AuthService {  
-  constructor(public jwtHelper: JwtHelperService) {
+export class AuthService { 
 
+  constructor() {
   }  
   
   // ...
@@ -15,6 +14,6 @@ export class AuthService {
     const token = localStorage.getItem('token');    
     // Check whether the token is expired and return
     // true or false
-    return !this.jwtHelper.isTokenExpired(token);
+    return Boolean(token);
   }
 }

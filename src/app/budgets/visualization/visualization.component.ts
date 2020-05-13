e36d1e63d508900559caf333cc27ad94fe4ac390
Jonathan Ho <johnnyho82@gmail.com>
2020-05-13 19:50:51 -0400

    Update styles, add tabs to Budget Form

--- src/app/budgets/visualization/visualization.component.ts
+++ src/app/budgets/visualization/visualization.component.ts
@@ -3,16 +3,16 @@ import * as CanvasJS from '../../../assets/scripts/canvasjs.min';
 @Component({
   selector: 'app-visualization',
   templateUrl: './visualization.component.html',
-  styleUrls: ['./visualization.component.css']
+  styleUrls: ['./visualization.component.css'],
 })
 export class VisualizationComponent implements OnInit {
   @Input() _budget: any = {};
   chart: CanvasJS.Chart;
-  constructor() { }
+  constructor() {}
 
   get budget(): any {
     this.updateChart();
-    return this._budget
+    return this._budget;
   }
 
   @Input() set budget(val: any) {
@@ -27,31 +27,74 @@ export class VisualizationComponent implements OnInit {
 
   createData(): any[] {
     const datapoints = [
-      {y: Object.values(this._budget.food).reduce((val: number, cur: number) => cur + val, 0), name: "Food"},
-      {y: Object.values(this._budget.personal_expenses).reduce((val: number, cur: number) => cur + val, 0), name: "Personal Expenses"},
-      {y: Object.values(this._budget.medical).reduce((val: number, cur: number) => cur + val, 0), name: "Medical"},
-      {y: Object.values(this._budget.housing).reduce((val: number, cur: number) => cur + val, 0), name: "Housing"},
-      {y: Object.values(this._budget.transportation).reduce((val: number, cur: number) => cur + val, 0), name: "Transportation"},
-      {y: Object.values(this._budget.savings).reduce((val: number, cur: number) => cur + val, 0), name: "Savings"},
-      {y: Object.values(this._budget.other_necessary).reduce((val: number, cur: number) => cur + val, 0), name: "Other"},
-    ]
+      {
+        y: Object.values(this._budget.food).reduce(
+          (val: number, cur: number) => cur + val,
+          0
+        ),
+        name: 'Food',
+      },
+      {
+        y: Object.values(this._budget.personal_expenses).reduce(
+          (val: number, cur: number) => cur + val,
+          0
+        ),
+        name: 'Personal Expenses',
+      },
+      {
+        y: Object.values(this._budget.medical).reduce(
+          (val: number, cur: number) => cur + val,
+          0
+        ),
+        name: 'Medical',
+      },
+      {
+        y: Object.values(this._budget.housing).reduce(
+          (val: number, cur: number) => cur + val,
+          0
+        ),
+        name: 'Housing',
+      },
+      {
+        y: Object.values(this._budget.transportation).reduce(
+          (val: number, cur: number) => cur + val,
+          0
+        ),
+        name: 'Transportation',
+      },
+      {
+        y: Object.values(this._budget.savings).reduce(
+          (val: number, cur: number) => cur + val,
+          0
+        ),
+        name: 'Savings',
+      },
+      {
+        y: Object.values(this._budget.other_necessary).reduce(
+          (val: number, cur: number) => cur + val,
+          0
+        ),
+        name: 'Other',
+      },
+    ];
     return datapoints;
-  
   }
-  
+
   ngOnInit(): void {
-    this.chart = new CanvasJS.Chart("chartContainer", {
+    this.chart = new CanvasJS.Chart('chartContainer', {
       animationEnabled: true,
       exportEnabled: true,
       title: {
-        text: "Your spendings"
+        text: 'Your Expenses',
       },
-      data: [{
-        type: "pie",
-        showInLegend: true,
-        toolTipContent: "<b>{name}</b>: ${y} (#percent%)",
-        indexLabel: "{name} - #percent%",
-      }]
+      data: [
+        {
+          type: 'pie',
+          showInLegend: true,
+          toolTipContent: '<b>{name}</b>: ${y} (#percent%)',
+          // indexLabel: '{name} - #percent%',
+        },
+      ],
     });
     this.chart.render();
   }

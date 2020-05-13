bdcef0c174611f3892f1b723d35da22f452dabda
Jordan <jordandoan@hotmail.com>
2020-05-13 15:33:57 -0700

    added data from budget to pie chart

--- src/app/budgets/visualization/visualization.component.ts
+++ src/app/budgets/visualization/visualization.component.ts
@@ -18,30 +18,41 @@ export class VisualizationComponent implements OnInit {
   @Input() set budget(val: any) {
     this._budget = val;
   }
-  
+
   updateChart(): void {
-    this.chart.options.data[0].dataPoints = [
-      { y: this._budget.food.groceries, label: "Groceries" },
-    ];
+    const datapoints = this.createData();
+    this.chart.options.data[0].dataPoints = datapoints;
     this.chart.render();
   }
 
+  createData(): any[] {
+    const datapoints = [
+      {y: Object.values(this._budget.food).reduce((val: number, cur: number) => cur + val, 0), name: "Food"},
+      {y: Object.values(this._budget.personal_expenses).reduce((val: number, cur: number) => cur + val, 0), name: "Personal Expenses"},
+      {y: Object.values(this._budget.medical).reduce((val: number, cur: number) => cur + val, 0), name: "Medical"},
+      {y: Object.values(this._budget.housing).reduce((val: number, cur: number) => cur + val, 0), name: "Housing"},
+      {y: Object.values(this._budget.transportation).reduce((val: number, cur: number) => cur + val, 0), name: "Transportation"},
+      {y: Object.values(this._budget.savings).reduce((val: number, cur: number) => cur + val, 0), name: "Savings"},
+      {y: Object.values(this._budget.other_necessary).reduce((val: number, cur: number) => cur + val, 0), name: "Other"},
+    ]
+    return datapoints;
+  
+  }
+  
   ngOnInit(): void {
     this.chart = new CanvasJS.Chart("chartContainer", {
-
       animationEnabled: true,
-      // exportEnabled: true,
+      exportEnabled: true,
       title: {
         text: "Your spendings"
       },
       data: [{
-        type: "column",
-        dataPoints: [
-          { y: this._budget.food.groceries, label: "Groceries" },
-        ]
+        type: "pie",
+        showInLegend: true,
+        toolTipContent: "<b>{name}</b>: ${y} (#percent%)",
+        indexLabel: "{name} - #percent%",
       }]
     });
+    this.chart.render();
   }
-
-
 }
